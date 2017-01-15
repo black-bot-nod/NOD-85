77857d4ac8a2814808ea959673c62e34770851f7
black-bot-nod <mardiofroham@gmail.com>
2017-01-15 15:30:10 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -247,7 +247,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {320244588},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
